00ab1ed91f8e0591b8037ab859d433bfc89adcca
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-01-03 11:16:27 +0200

    add module for app routing
    Former-commit-id: 64a894b8ce979ad9baf72ff24e76f5c9b3e1732c

--- CompeteAI/src/app/app.module.ts
+++ CompeteAI/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
