7cbe3e8f11f4d1e482efade8d7583055ea7bda5b
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-01-03 11:29:08 +0200

    add component for home and navmenu
    Former-commit-id: 401aeb2d0a88f1fa60f14adfbb6060ced869b484

--- CompeteAI/src/app/app.component.ts
+++ CompeteAI/src/app/app.component.ts
@@ -8,20 +8,7 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
-
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
+  
 
   title = 'CompeteAI';
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
